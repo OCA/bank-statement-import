52aa9aa64362577e180ce583e50356611d38ed61
Ronald Portier (Therp BV) <ronald@therp.nl>
2014-12-15 10:18:55 +0100

    [RFR] Move backported bank import modules from bank-payment     to bank-statement-import repository.

---
+++ account_bank_statement_import_qif/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# noqa: This is a backport from Odoo. OCA has no control over style here.
+# flake8: noqa
+from . import test_import_bank_statement
+checks = [
+    test_import_bank_statement
+]
+
