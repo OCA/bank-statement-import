c9cbcac6cf0635b092d55550cbb0602eea79d1f1
Ronald Portier (Therp BV) <ronald@therp.nl>
2014-12-21 14:20:09 +0100

    [FIX] Forgot to commit some __Init__.py files.

--- account_bank_statement_import/__init__.py
+++ account_bank_statement_import/__init__.py
@@ -1,7 +1,4 @@
 # -*- encoding: utf-8 -*-
-# noqa: This is a backport from Odoo. OCA has no control over style here.
-# flake8: noqa
-
-import account_bank_statement_import
+from . import account_bank_statement_import
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
