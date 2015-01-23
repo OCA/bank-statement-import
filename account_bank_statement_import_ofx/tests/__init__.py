8cc1a5229cf985491e13bb21f1bdb30096ceb446
Alexis de Lattre <alexis@via.ecp.fr>
2015-01-23 22:50:24 +0100

    New backport from odoo/master Fix bug #5

--- account_bank_statement_import_ofx/tests/__init__.py
+++ account_bank_statement_import_ofx/tests/__init__.py
@@ -2,7 +2,3 @@
 # noqa: This is a backport from Odoo. OCA has no control over style here.
 # flake8: noqa
 from . import test_import_bank_statement
-
-checks = [
-        test_import_bank_statement
-]
