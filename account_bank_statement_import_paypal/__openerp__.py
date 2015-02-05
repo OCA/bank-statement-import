512bb9753a99e7045683e3de98d7c27ef047a6c7
Alexis de Lattre <alexis@via.ecp.fr>
2015-02-05 14:41:27 +0100

    Add module account_bank_statement_import_paypal Fix spelling mistake Add methods to inherit for language-specific or country-specific stuff Port to new API FIX super Update travis config

---
+++ account_bank_statement_import_paypal/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    account_bank_statement_import_paypal module for Odoo
+#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Import Paypal Bank Statements',
+    'version': '0.1',
+    'license': 'AGPL-3',
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'summary': 'Import Paypal CSV files as Bank Statements in Odoo',
+    'depends': ['account_bank_statement_import'],
+    'external_dependencies': {'python': ['unicodecsv']},
+    'data': [],
+    'installable': True,
+}
