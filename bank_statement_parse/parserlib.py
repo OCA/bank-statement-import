583764c16768c0abe67a8c29bbc111f2f2987611
Ronald Portier (Therp BV) <rportier@therp.nl>
2015-03-28 13:36:37 +0100

    [PEP] Add 2 blank lines in parserlib.py.

--- bank_statement_parse/parserlib.py
+++ bank_statement_parse/parserlib.py
@@ -19,6 +19,8 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
+
 class BankStatement(object):
     """A bank statement groups data about several bank transactions."""
 
