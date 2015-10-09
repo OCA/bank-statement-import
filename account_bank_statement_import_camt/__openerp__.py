c885e14f6c1099547b3a1354d55b5c2494f62edb
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 09:59:36 +0200

    [UPD] prefix versions with 8.0

--- account_bank_statement_import_camt/__openerp__.py
+++ account_bank_statement_import_camt/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'CAMT Format Bank Statements Import',
-    'version': '0.3',
+    'version': '8.0.0.3.0',
     'license': 'AGPL-3',
     'author': 'Odoo Community Association (OCA), Therp BV',
     'website': 'https://github.com/OCA/bank-statement-import',
