dd0651991b00e82e77cd21e6e1581401feda95bd
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 09:59:36 +0200

    [UPD] prefix versions with 8.0

--- account_bank_statement_import_ofx/__openerp__.py
+++ account_bank_statement_import_ofx/__openerp__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Import OFX Bank Statement',
     'category': 'Banking addons',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'author': 'OpenERP SA,'
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/bank-statement-import',
