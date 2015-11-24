855b458b115e5b502fd3dad26549499ec7562deb
Ilyas <rakhimkulov@it-projects.info>
2015-11-24 13:44:08 +0500

    open-erp some not important changes

--- account_bank_statement_import_1c/__openerp__.py
+++ account_bank_statement_import_1c/__openerp__.py
@@ -3,7 +3,7 @@
     'name': 'Import 1c Bank Statement',
     'category': 'Banking addons',
     'version': '9.0.1.0.0',
-    'author': 'IT Projects',
+    'author': 'IT-Projects LLC, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/bank-statement-import',
     'depends': ['account_bank_statement_import'],
     'auto_install': False,
