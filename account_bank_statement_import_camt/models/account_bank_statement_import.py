7d5ef65727a19918486d50a32d6e3b2f87cca3a0
Ronald Portier <ronald@therp.nl>
2016-10-21 10:58:14 +0200

    [FIX] Minimize changes for enhanced unique id.

--- account_bank_statement_import_camt/models/account_bank_statement_import.py
+++ account_bank_statement_import_camt/models/account_bank_statement_import.py
@@ -1,24 +1,8 @@
 # -*- coding: utf-8 -*-
-"""Add process_camt method to account.bank.statement.import."""
-##############################################################################
-#
-#    Copyright (C) 2013-2015 Therp BV <http://therp.nl>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2013-2017 Therp BV <http://therp.nl>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import logging
+
 from openerp import models
 
 _logger = logging.getLogger(__name__)
@@ -36,7 +20,8 @@ class AccountBankStatementImport(models.TransientModel):
             return parser.parse(data_file)
         except ValueError:
             # Not a camt file, returning super will call next candidate:
-            _logger.debug("Statement file was not a camt file.",
-                          exc_info=True)
+            _logger.debug(
+                "Statement file was not a camt file.", exc_info=True
+            )
             return super(AccountBankStatementImport, self)._parse_file(
                 cr, uid, data_file, context=context)
