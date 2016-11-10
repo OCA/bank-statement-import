6e143fa4ffbae1afc82091bb1a83787e5fa6036a
Holger Brunn <hbrunn@therp.nl>
2016-11-10 17:18:30 +0100

    migrate account_bank_statement_import_camt

--- account_bank_statement_import_camt/tests/test_import_bank_statement.py
+++ account_bank_statement_import_camt/tests/test_import_bank_statement.py
@@ -1,52 +1,54 @@
 # -*- coding: utf-8 -*-
 """Run test to import camt.053 import."""
-##############################################################################
-#
-#    Copyright (C) 2015 Therp BV <http://therp.nl>.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp.addons.account_bank_statement_import.tests import (
-    TestStatementFile)
+# © 2013-2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import base64
+from openerp.tests.common import TransactionCase
+from openerp.tools.misc import file_open
 
 
-class TestImport(TestStatementFile):
+class TestImport(TransactionCase):
     """Run test to import camt import."""
+    transactions = [
+        {
+            'account_number': 'NL46ABNA0499998748',
+            'amount': -754.25,
+            'date': '2014-01-05',
+            'ref': '435005714488-ABNO33052620',
+        },
+    ]
 
     def test_statement_import(self):
         """Test correct creation of single statement."""
-        transactions = [
-            {
-                'remote_account': 'NL46ABNA0499998748',
-                'transferred_amount': -754.25,
-                'value_date': '2014-01-05',
-                'ref': '435005714488-ABNO33052620',
-            },
-        ]
-        self._test_statement_import(
-            'account_bank_statement_import_camt', 'test-camt053.xml',
-            '1234Test/1',
-            local_account='NL77ABNA0574908765',
-            start_balance=15568.27, end_balance=15121.12,
-            transactions=transactions
-        )
+        action = {}
+        with file_open(
+            'account_bank_statement_import_camt/test_files/test-camt053'
+        ) as testfile:
+            action = self.env['account.bank.statement.import'].create({
+                'data_file': base64.b64encode(testfile.read()),
+            }).import_file()
+        for statement in self.env['account.bank.statement'].browse(
+            action['context']['statement_ids']
+        ):
+            self.assertTrue(any(
+                all(
+                    line[key] == self.transactions[0][key]
+                    for key in ['amount', 'date', 'ref']
+                ) and
+                line.bank_account_id.acc_number ==
+                self.transactions[0]['account_number']
+                for line in statement.line_ids
+            ))
 
     def test_zip_import(self):
         """Test import of multiple statements from zip file."""
-        self._test_statement_import(
-            'account_bank_statement_import_camt', 'test-camt053.zip',
-            '1234Test/2',  # Only name of first statement
-            local_account='NL77ABNA0574908765',
-        )
+        with file_open(
+            'account_bank_statement_import_camt/test_files/test-camt053.zip'
+        ) as testfile:
+            action = self.env['account.bank.statement.import'].create({
+                'data_file': base64.b64encode(testfile.read()),
+            }).import_file()
+        for statement in self.env['account.bank.statement'].browse(
+            action['context']['statement_ids']
+        ):
+            self.assertTrue(statement.line_ids)
