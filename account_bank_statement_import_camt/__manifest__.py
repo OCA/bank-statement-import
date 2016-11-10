261f3a0213b972abbe1da6c83de1851ad8122163
Holger Brunn <hbrunn@therp.nl>
2016-11-10 17:18:30 +0100

    migrate account_bank_statement_import_camt

--- account_bank_statement_import_camt/__manifest__.py
+++ account_bank_statement_import_camt/__manifest__.py
@@ -1,25 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2013-2015 Therp BV <http://therp.nl>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2013-2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'CAMT Format Bank Statements Import',
-    'version': '8.0.0.4.0',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Odoo Community Association (OCA), Therp BV',
     'website': 'https://github.com/OCA/bank-statement-import',
@@ -30,5 +14,7 @@
     'demo': [
         'demo/demo_data.xml',
     ],
-    'installable': False,
+    'data': [
+        'views/account_bank_statement_import.xml',
+    ],
 }
---
+++ account_bank_statement_import_camt/models/account_bank_statement_import.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""Add process_camt method to account.bank.statement.import."""
+# © 2013-2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import logging
+import StringIO
+import zipfile
+from openerp import api, models
+from ..camt import CamtParser as Parser
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountBankStatementImport(models.TransientModel):
+    """Add process_camt method to account.bank.statement.import."""
+    _inherit = 'account.bank.statement.import'
+
+    @api.model
+    def _parse_file(self, data_file):
+        """Parse a CAMT053 XML file."""
+        try:
+            parser = Parser()
+            _logger.debug("Try parsing with camt.")
+            return parser.parse(data_file)
+        except ValueError:
+            try:
+                with zipfile.ZipFile(StringIO.StringIO(data_file)) as data:
+                    currency = None
+                    account_number = None
+                    transactions = []
+                    for member in data.namelist():
+                        currency, account_number, new = self._parse_file(
+                            data.open(member).read()
+                        )
+                        transactions.extend(new)
+                return currency, account_number, transactions
+            except (zipfile.BadZipfile, ValueError):
+                pass
+            # Not a camt file, returning super will call next candidate:
+            _logger.debug("Statement file was not a camt file.",
+                          exc_info=True)
+        return super(AccountBankStatementImport, self)._parse_file(data_file)
