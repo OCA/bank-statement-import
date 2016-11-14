4f95ebae61166dfd8be12a7a89440ace35d8fc08
Alexis de Lattre <alexis.delattre@akretion.com>
2016-11-14 17:21:14 +0100

    [MIG] account_bank_statement_import_ofx: Migration to 10.0
    * Remove the code that matches partners, which is wrong and cannot work
    * Better 'name' on bank statement
    * Move from models directory to wizard
    * Remove demo data to tests
    * Use latest version of ofxparse from github

--- account_bank_statement_import_ofx/__manifest__.py
+++ account_bank_statement_import_ofx/__manifest__.py
@@ -2,26 +2,22 @@
 {
     'name': 'Import OFX Bank Statement',
     'category': 'Banking addons',
-    'version': '9.0.0.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'OpenERP SA,'
+    'author': 'Odoo SA,'
+              'Akretion,'
               'La Louve,'
               'GRAP,'
               'Odoo Community Association (OCA)',
     'website': 'https://odoo-community.org/',
     'depends': [
-        'l10n_generic_coa',
         'account_bank_statement_import',
     ],
     'data': [
         'views/view_account_bank_statement_import.xml',
     ],
-    'demo': [
-        'demo/demo_data.xml',
-    ],
     'external_dependencies': {
         'python': ['ofxparse'],
     },
-    'auto_install': False,
     'installable': True,
 }
