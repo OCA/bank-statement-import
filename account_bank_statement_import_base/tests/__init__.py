b894a46b03d1483a22d287180dfa0d127e16a8a0
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-12-06 12:52:49 +0100

    [REM] account_bank_statement_import: Removed as already included in v9

--- account_bank_statement_import_base/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""Define tests to be run."""
-from . import test_res_partner_bank
-from . import test_import_bank_statement
-from .test_import_file import TestStatementFile
