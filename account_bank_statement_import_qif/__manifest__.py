b77e52c49820ca852ad68ea8f62fb0ac8f260bbf
Sébastien Namèche <sebastien.nameche@essor-rdc.org>
2017-01-08 09:24:52 +0100

    [10.0] [MIG] account_bank_statement_import_qif

--- account_bank_statement_import_qif/__manifest__.py
+++ account_bank_statement_import_qif/__manifest__.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
+# Copyright 2015 Odoo S. A.
+# Copyright 2015 Laurent Mignon <laurent.mignon@acsone.eu>
+# Copyright 2015 Ronald Portier <rportier@therp.nl>
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Import QIF Bank Statement',
-    'category': 'Banking addons',
-    'version': '8.0.1.0.0',
+    'name': 'Import QIF Bank Statements',
+    'category': 'Accounting',
+    'version': '10.0.1.0.0',
     'author': 'OpenERP SA,'
+              'Tecnativa,'
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/bank-statement-import',
-    'images': [],
     'depends': [
-        'account_bank_statement_import'
+        'account_bank_statement_import',
     ],
-    'auto_install': False,
-    'installable': False,
+    'data': [
+        'wizards/account_bank_statement_import_qif_view.xml',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
 }
