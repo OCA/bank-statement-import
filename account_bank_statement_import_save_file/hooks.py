6cb100b652e20d6b982ebbd2d0a00f24d9d5107c
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-24 12:12:12 +0100

    account_bank_statement_import_save_file: Migrated to 10.0

--- account_bank_statement_import_save_file/hooks.py
+++ account_bank_statement_import_save_file/hooks.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2015 Therp BV <http://therp.nl>.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import SUPERUSER_ID
+# © 2015 Therp BV (<http://therp.nl>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
 def _post_init_hook(cr, pool):
@@ -89,7 +71,6 @@ def _post_init_hook_migrate_account_banking_imported_file(cr, pool):
         (tuple(attachment_ids),)
     )
     for attachment_id, content in cr.fetchall():
-        pool['ir.attachment'].write(
-            cr, SUPERUSER_ID,
+        pool['ir.attachment'].sudo().write(
             [attachment_id],
             {'datas': str(content)})
--- account_bank_statement_import_save_file/models/account_bank_statement_import.py
+++ account_bank_statement_import_save_file/models/account_bank_statement_import.py
@@ -1,41 +1,28 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Therp BV (<http://therp.nl>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 import base64
-from openerp import models, api
+from odoo import models, api
 
 
 class AccountBankStatementImport(models.TransientModel):
     _inherit = 'account.bank.statement.import'
 
     @api.model
-    def _import_file(self, data_file):
-        (statement_ids, notifications) = \
-            super(AccountBankStatementImport, self)._import_file(data_file)
+    def import_file(self):
+        action = \
+            super(AccountBankStatementImport, self).import_file()
+        statement_ids = action.get('context', {}).get('statement_ids')
+        notifications = action.get('context', {}).get('notifications')
+        data_file = base64.b64decode(self.data_file)
         if statement_ids:
             self.env['account.bank.statement'].browse(statement_ids).write({
                 'import_file': self.env['ir.attachment'].create(
                     self._create_import_file_attachment_data(
                         data_file, statement_ids[0], notifications)).id,
             })
-        return (statement_ids, notifications)
+        return action
 
     @api.model
     def _create_import_file_attachment_data(self, data_file, statement_id,
