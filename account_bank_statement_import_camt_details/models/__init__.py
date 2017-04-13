25980519fe616f28bed69a99f376f4a3990ac6f6
Emanuel Cino <ecino@compassion.ch>
2017-04-13 11:47:46 +0200

    [ADD] camt_details bank statement import

---
+++ account_bank_statement_import_camt_details/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2017 Compassion CH <http://www.compassion.ch>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import account_bank_statement_line
+from . import camt_parser
