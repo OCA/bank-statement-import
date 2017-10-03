44650b3000cb9a573d7b3f9fb518805551f6d0d1
Andrius Preimantas <andrius@versada.lt>
2017-10-03 11:58:52 +0300

    account_bank_statement_import_camt: properly parse statement date

--- account_bank_statement_import_camt/camt.py
+++ account_bank_statement_import_camt/camt.py
@@ -4,6 +4,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import re
 from lxml import etree
+import dateutil
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 
 
 class CamtParser(object):
@@ -170,6 +172,10 @@ class CamtParser(object):
             ns, node, './ns:Id', result, 'name')
         self.add_value_from_node(
             ns, node, './ns:CreDtTm', result, 'date')
+        # CreDtTm comes in ISO 8601 format which needs to be parsed further
+        # We intentionally fail import if parsing fails
+        result['date'] = dateutil.parser.parse(
+            result['date']).date().strftime(DEFAULT_SERVER_DATE_FORMAT)
         self.add_value_from_node(
             ns, node, './ns:Acct/ns:Ccy', result, 'currency')
         result['balance_start'], result['balance_end_real'] = (
