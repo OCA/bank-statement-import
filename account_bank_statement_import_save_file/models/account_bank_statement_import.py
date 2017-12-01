22e94476b2bca47e12ac36db7a006912aee3dece
Alexis de Lattre <alexis.delattre@akretion.com>
2017-12-01 19:26:53 +0100

    account_bank_statement_import_save_file: Fix filename

--- account_bank_statement_import_save_file/models/account_bank_statement_import.py
+++ account_bank_statement_import_save_file/models/account_bank_statement_import.py
@@ -20,19 +20,21 @@ class AccountBankStatementImport(models.TransientModel):
             self.env['account.bank.statement'].browse(statement_ids).write({
                 'import_file': self.env['ir.attachment'].create(
                     self._create_import_file_attachment_data(
-                        data_file, statement_ids[0], notifications)).id,
+                        data_file, statement_ids[0], notifications,
+                        self.filename)).id,
             })
         return action
 
     @api.model
     def _create_import_file_attachment_data(self, data_file, statement_id,
-                                            notifications):
+                                            notifications, filename=None):
         return {
-            'name': '<unknown>',
+            'name': filename or '<unknown>',
             'res_model': 'account.bank.statement',
             'res_id': statement_id,
             'type': 'binary',
             'datas': base64.b64encode(data_file),
+            'datas_fname': filename or '<unknown>',
             'description': '\n'.join(
                 '%(type)s: %(message)s' % notification
                 for notification in notifications) or False,
