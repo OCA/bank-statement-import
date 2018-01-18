f8abc3d359fd558da2325d5b5a4b8029cdd0ec2c
Nicolas JEUDY <nicolas@panda-chi.io>
2018-01-18 14:34:51 +0100

    [MIG] migrate account_bank_statement_import_ofx to V11
    - add #136 patch

--- account_bank_statement_import_ofx/__manifest__.py
+++ account_bank_statement_import_ofx/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'Import OFX Bank Statement',
     'category': 'Banking addons',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Odoo SA,'
               'Akretion,'
               'La Louve,'
               'GRAP,'
+              'Nicolas JEUDY,'
               'Odoo Community Association (OCA)',
     'website': 'https://odoo-community.org/',
     'depends': [
