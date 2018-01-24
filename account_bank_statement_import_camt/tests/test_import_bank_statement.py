c4dc160eda0b817d0f64cd647cbdedf64446620d
ecino <ecino@users.noreply.github.com>
2018-01-24 15:11:26 +0100

    [10.0] Better parse camt groupped transactions (#131)
    * correctly parse entries with multiple transactions
    
    Our bank does it.  Care has been taken not to break cases where
    Ntry/NtryDtls/TxDtls isn't used: if any piece of information isn't found
    on these nodes, the matching information from the Ntry is used.  Most
    often the entry has a sum or a summary and the transaction details are
    more precise and specific, so it makes sense to use their contents
    rather than its.
    
    * more complete unit tests for camt parser
    
    This checks the whole output data of the parser against its expected
    output.
    
    * add a test case that checks TxDtls decoding
    
    * special case for Ntrys without TxDtls
    
    * CO-12 - Date Import BVR
    
    * FIX tests
    
    * CO-1232 Fixed issues with CAMT parser tests

--- account_bank_statement_import_camt/tests/test_import_bank_statement.py
+++ account_bank_statement_import_camt/tests/test_import_bank_statement.py
@@ -1,10 +1,51 @@
 # -*- coding: utf-8 -*-
 """Run test to import camt.053 import."""
 # © 2013-2016 Therp BV <http://therp.nl>
+# Copyright 2017 Open Net Sàrl
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import base64
+import difflib
+import pprint
+import tempfile
+
 from odoo.tests.common import TransactionCase
 from odoo.tools.misc import file_open
+
+
+DATA_DIR = 'account_bank_statement_import_camt/test_files/'
+
+
+class TestParser(TransactionCase):
+    """Tests for the camt parser itself."""
+    def setUp(self):
+        super(TestParser, self).setUp()
+        self.parser = self.env['account.bank.statement.import.camt.parser']
+
+    def _do_parse_test(self, inputfile, goldenfile):
+        with file_open(inputfile) as testfile:
+            data = testfile.read()
+        res = self.parser.parse(data)
+        with tempfile.NamedTemporaryFile(suffix='.pydata') as temp:
+            pprint.pprint(res, temp)
+            with file_open(goldenfile) as golden:
+                temp.seek(0)
+                diff = list(
+                    difflib.unified_diff(golden.readlines(), temp.readlines(),
+                                         golden.name,        temp.name))
+                if len(diff) > 2:
+                    self.fail(
+                        "actual output doesn't match exptected output:\n%s" %
+                        "".join(diff))
+
+    def test_parse(self):
+        self._do_parse_test(
+            DATA_DIR + 'test-camt053',
+            DATA_DIR + 'golden-camt053.pydata')
+
+    def test_parse_txdtls(self):
+        self._do_parse_test(
+            DATA_DIR + 'test-camt053-txdtls',
+            DATA_DIR + 'golden-camt053-txdtls.pydata')
 
 
 class TestImport(TransactionCase):
@@ -36,9 +77,7 @@ class TestImport(TransactionCase):
     def test_statement_import(self):
         """Test correct creation of single statement."""
         action = {}
-        with file_open(
-            'account_bank_statement_import_camt/test_files/test-camt053'
-        ) as testfile:
+        with file_open(DATA_DIR + 'test-camt053') as testfile:
             action = self.env['account.bank.statement.import'].create({
                 'data_file': base64.b64encode(testfile.read()),
             }).import_file()
