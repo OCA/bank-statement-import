f71ec839a3436a42c49a527a801e818e7c5d25ab
Maxence Groine <mgroine@fiefmanage.ch>
2018-06-01 15:10:44 +0200

    Rename module
    Also:
    - Use context managers for file opening/closing in tests
    - Changed use of deprecated `BadZipfile` exception
    - Removed debugging code left by mistake

---
+++ account_bank_statement_import_camt_oca/models/__init__.py
@@ -0,0 +1,4 @@
+# © 2013-2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import parser
+from . import account_bank_statement_import
---
+++ account_bank_statement_import_camt_oca/tests/__init__.py
@@ -0,0 +1,4 @@
+"""Test import of bank statement for camt.053."""
+# © 2013-2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import test_import_bank_statement
