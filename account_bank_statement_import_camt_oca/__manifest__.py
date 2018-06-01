cb29db252def3e4db86caa3c36f53b95d4da1ee3
Maxence Groine <mgroine@fiefmanage.ch>
2018-06-01 15:10:44 +0200

    Rename module
    Also:
    - Use context managers for file opening/closing in tests
    - Changed use of deprecated `BadZipfile` exception
    - Removed debugging code left by mistake

---
+++ account_bank_statement_import_camt_oca/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2013-2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'CAMT Format Bank Statements Import',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Odoo Community Association (OCA), Therp BV',
+    'website': 'https://github.com/OCA/bank-statement-import',
+    'category': 'Banking addons',
+    'depends': [
+        'account_bank_statement_import',
+    ],
+    'data': [
+        'views/account_bank_statement_import.xml',
+    ],
+}
