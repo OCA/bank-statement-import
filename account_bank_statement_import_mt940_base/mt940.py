15531fef4c8c1b783499561ac2a22806fc4004e0
Ronald Portier <ronald@therp.nl>
2018-06-08 15:10:01 +0200

    [FIX] account_bank_statement_import_mt940_base. Add tests.
    Also incremenent module version and use shortened headers.

--- account_bank_statement_import_mt940_base/mt940.py
+++ account_bank_statement_import_mt940_base/mt940.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
 """Generic parser for MT940 files, base for customized versions per bank."""
-##############################################################################
-#
-#    Copyright (C) 2014-2015 Therp BV <http://therp.nl>.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014-2018 Therp BV <https://therp.nl>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import re
 import logging
 from datetime import datetime
