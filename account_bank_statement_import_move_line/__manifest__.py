a22ec2a2b647809c4cb9861c258f20ea0dd480e3
Sergej Lozikov <sergej.lozikov@xpansa.com>
2018-08-03 00:04:54 +0300

    [MIG] account_bank_statement_import_move_line: Migration to 11.0

--- account_bank_statement_import_move_line/__manifest__.py
+++ account_bank_statement_import_move_line/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Luis M. Ontalba
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Bank statement import move lines',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting',
+    'summary': 'Import journal items into bank statement',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
     'website': 'https://www.tecnativa.com',
