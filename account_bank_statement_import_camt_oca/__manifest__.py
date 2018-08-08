64f26f3a6b98bc82f7c0e2fa24e209ffd1839162
Andrea Stirpe <a.stirpe@onestein.nl>
2018-08-08 08:14:07 +0200

    [11.0][FIX] account_bank_statement_import_camt: payment amounts (#168)

--- account_bank_statement_import_camt_oca/__manifest__.py
+++ account_bank_statement_import_camt_oca/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'CAMT Format Bank Statements Import',
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.0.2',
     'license': 'AGPL-3',
     'author': 'Odoo Community Association (OCA), Therp BV',
     'website': 'https://github.com/OCA/bank-statement-import',
