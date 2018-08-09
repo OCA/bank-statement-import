cbb05d956fd0c7ca410e2f72cb684dcc1a131c21
oleksandrpaziuk <o.paziuk@mobilunity.com>
2018-08-09 13:36:30 +0300

    [10.0][FIX] fix disambiguation in account_bank_statement_import_camt module name

--- account_bank_statement_import_mt940_base/mt940.py
+++ account_bank_statement_import_mt940_base/mt940.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2014-2015 Therp BV <http://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 """Generic parser for MT940 files, base for customized versions per bank."""
