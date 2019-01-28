2922304f928a6baf1c8005a49692f917b295ce94
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2019-01-28 15:13:21 +0100

    [MIG] Account Bank Statement Import CAMT OCA to 12.0

--- account_bank_statement_import_camt_oca/__init__.py
+++ account_bank_statement_import_camt_oca/__init__.py
@@ -1,3 +1,1 @@
-# © 2013-2016 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
--- account_bank_statement_import_camt_oca/tests/test_import_bank_statement.py
+++ account_bank_statement_import_camt_oca/tests/test_import_bank_statement.py
@@ -1,8 +1,8 @@
-"""Run test to import camt.053 import."""
-# © 2013-2016 Therp BV <http://therp.nl>
+# Copyright 2013-2016 Therp BV <https://therp.nl>
 # Copyright 2017 Open Net Sàrl
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 import base64
+from datetime import date
 import difflib
 import pprint
 import tempfile
@@ -69,25 +69,25 @@ class TestImport(TransactionCase):
         {
             'account_number': 'NL46ABNA0499998748',
             'amount': -754.25,
-            'date': '2014-01-05',
+            'date': date(year=2014, month=1, day=5),
             'ref': '435005714488-ABNO33052620',
         },
         {
             'remote_account': 'NL46ABNA0499998748',
             'transferred_amount': -564.05,
-            'value_date': '2014-01-05',
+            'value_date': date(year=2014, month=1, day=5),
             'ref': 'TESTBANK/NL/20141229/01206408',
         },
         {
             'remote_account': 'NL46ABNA0499998748',
             'transferred_amount': -100.0,
-            'value_date': '2014-01-05',
+            'value_date': date(year=2014, month=1, day=5),
             'ref': 'TESTBANK/NL/20141229/01206407',
         },
         {
             'remote_account': 'NL69ABNA0522123643',
             'transferred_amount': 1405.31,
-            'value_date': '2014-01-05',
+            'value_date': date(year=2014, month=1, day=5),
             'ref': '115',
         },
     ]
@@ -96,6 +96,12 @@ class TestImport(TransactionCase):
         super(TestImport, self).setUp()
         bank = self.env['res.partner.bank'].create({
             'acc_number': 'NL77ABNA0574908765',
+            'partner_id': self.env.ref('base.main_partner').id,
+            'company_id': self.env.ref('base.main_company').id,
+            'bank_id': self.env.ref('base.res_bank_1').id,
+        })
+        self.env['res.partner.bank'].create({
+            'acc_number': 'NL46ABNA0499998748',
             'partner_id': self.env.ref('base.main_partner').id,
             'company_id': self.env.ref('base.main_company').id,
             'bank_id': self.env.ref('base.res_bank_1').id,
@@ -119,18 +125,18 @@ class TestImport(TransactionCase):
                 'data_file': base64.b64encode(datafile.read())
             }).import_file()
 
-            for statement in self.env['account.bank.statement'].browse(
+            statement_lines = self.env['account.bank.statement'].browse(
                 action['context']['statement_ids']
-            ):
-                self.assertTrue(any(
-                    all(
-                        line[key] == self.transactions[0][key]
-                        for key in ['amount', 'date', 'ref']
-                    ) and
-                    line.bank_account_id.acc_number ==
-                    self.transactions[0]['account_number']
-                    for line in statement.line_ids
-                ))
+            ).line_ids
+            self.assertTrue(any(
+                all(
+                    line[key] == self.transactions[0][key]
+                    for key in ['amount', 'date', 'ref']
+                ) and
+                line.bank_account_id.acc_number ==
+                self.transactions[0]['account_number']
+                for line in statement_lines
+            ))
 
     def test_zip_import(self):
         """Test import of multiple statements from zip file."""
@@ -145,6 +151,5 @@ class TestImport(TransactionCase):
             }).import_file()
 
             for statement in self.env['account.bank.statement'].browse(
-                action['context']['statement_ids']
-            ):
+                    action['context']['statement_ids']):
                 self.assertTrue(statement.line_ids)
