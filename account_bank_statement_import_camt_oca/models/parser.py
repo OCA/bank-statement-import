e18924848ab89cac875b1e8cebaaee31eda09d64
Stephane <eicher31@hotmail.com>
2019-07-16 10:12:33 +0200

    No update name during parsing <NtryDtls>
    If the field "name" of the module "account.bank.statement.line" is already  assigned during the parse of the element NtryDtls of the camt053 file, we don't update it to avoid to lose data.

--- account_bank_statement_import_camt_oca/models/parser.py
+++ account_bank_statement_import_camt_oca/models/parser.py
@@ -54,12 +54,13 @@ class CamtParser(models.AbstractModel):
     def parse_transaction_details(self, ns, node, transaction):
         """Parse TxDtls node."""
         # message
-        self.add_value_from_node(
-            ns, node, [
-                './ns:RmtInf/ns:Ustrd|./ns:RtrInf/ns:AddtlInf',
-                './ns:AddtlNtryInf',
-                './ns:Refs/ns:InstrId',
-            ], transaction, 'name', join_str='\n')
+        if transaction['name'] == '/':
+            self.add_value_from_node(
+                ns, node, [
+                    './ns:RmtInf/ns:Ustrd|./ns:RtrInf/ns:AddtlInf',
+                    './ns:AddtlNtryInf',
+                    './ns:Refs/ns:InstrId',
+                ], transaction, 'name', join_str='\n')
         # name
         self.add_value_from_node(
             ns, node, [
