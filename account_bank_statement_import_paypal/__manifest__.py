9a7b3658077c3298856ad5771ae2424f35497736
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-07-19 17:11:12 +0200

    [FIX] account_bank_statement_import_paypal: Don't populate partner account
    This makes the statement lines to not appear on reconciliation widget.

--- account_bank_statement_import_paypal/__manifest__.py
+++ account_bank_statement_import_paypal/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Import Paypal Bank Statements",
     'summary': 'Import Paypal CSV files as Bank Statements in Odoo',
-    "version": "11.0.2.0.0",
+    "version": "11.0.2.0.1",
     "category": "Accounting",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": " Akretion, Odoo Community Association (OCA)",
