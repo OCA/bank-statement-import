c42a4019a79d3794ed584f28079b7f73f60ee38a
Mihai Fekete <feketemihai@gmail.com>
2019-12-08 07:18:56 -0800

    Migrate import MT940 NL ING.

--- account_bank_statement_import_mt940_nl_ing/tests/test_import_bank_statement.py
+++ account_bank_statement_import_mt940_nl_ing/tests/test_import_bank_statement.py
@@ -1,53 +1,100 @@
 # -*- coding: utf-8 -*-
-"""Run test to import MT940 IBAN ING import."""
-##############################################################################
-#
-#    Copyright (C) 2015 Therp BV <http://therp.nl>.
-#
-#    All other contributions are (C) by their respective contributors
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp.addons.account_bank_statement_import.tests import (
-    TestStatementFile)
+# Copyright (C) 2013-2015 Therp BV <http://therp.nl>
+# Copyright 2019 OdooERP Romania <https://odooerpromania.ro>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import base64
+import datetime
+
+from odoo.tests.common import TransactionCase, tagged
+from odoo.modules.module import get_module_resource
 
 
-class TestImport(TestStatementFile):
+@tagged('-standard', 'mt940')
+class TestImport(TransactionCase):
     """Run test to import MT940 ING import."""
 
-    def test_old_statement_import(self):
-        """Test correct creation of single statement from old format."""
-        self._test_statement_import(
-            'account_bank_statement_import_mt940_nl_ing', 'test-ing-old.940',
-            'NL77INGB0574908765-2014-01-20',
-            start_balance=662.23, end_balance=564.35
-        )
-
-    def test_statement_import(self):
-        """Test correct creation of single statement."""
-        transactions = [
+    def setUp(self):
+        super(TestImport, self).setUp()
+        self.parser = self.env['account.bank.statement.import.mt940.parser']
+        self.bank_account = self.env['res.partner.bank'].create({
+            'acc_number': 'NL77INGB0574908765',
+            'partner_id': self.env.ref('base.main_partner').id,
+            'company_id': self.env.ref('base.main_company').id,
+            'bank_id': self.env.ref('base.res_bank_1').id,
+        })
+        self.journal = self.env['account.journal'].create({
+            'name': 'Bank Journal - (test ing mt940)',
+            'code': 'TBNK2MT940',
+            'type': 'bank',
+            'bank_account_id': self.bank_account.id,
+            'currency_id': self.env.ref('base.EUR').id,
+        })
+        self.transactions = [
             {
                 'remote_account': 'NL32INGB0000012345',
                 'transferred_amount': 1.56,
                 'value_date': '2014-02-20',
                 'ref': 'EV12341REP1231456T1234',
             },
+            {
+                'remote_account': 'NL32INGB0000012345',
+                'transferred_amount': 1.56,
+                'value_date': '2014-02-20',
+                'ref': 'EV12341REP1231456T1234',
+            }
         ]
-        self._test_statement_import(
-            'account_bank_statement_import_mt940_nl_ing', 'test-ing.940',
-            'NL77INGB0574908765-2014-02-20',
-            start_balance=662.23, end_balance=564.35,
-            transactions=transactions
+        self.data = \
+            "/BENM//NAME/Cost/REMI/Period 01-10-2013 t/m 31-12-2013/ISDT/20"
+        self.codewords = ['BENM', 'ADDR', 'NAME', 'CNTP', 'ISDT', 'REMI']
+
+    def test_get_subfields(self):
+        """Unit Test function get_subfields()."""
+
+        res = self.parser.get_subfields(self.data, self.codewords)
+        espected_res = {
+            'BENM': [''],
+            'NAME': ['Cost'],
+            'REMI': ['Period 01-10-2013 t', 'm 31-12-2013'],
+            'ISDT': ['20'],
+        }
+        self.assertEqual(res, espected_res)
+
+    def test_statement_import_ing(self):
+        """Test correct creation of single statement."""
+        testfile = get_module_resource(
+            'account_bank_statement_import_mt940_nl_ing',
+            'test_files',
+            'test-ing.940',
         )
+        with open(testfile, 'rb') as datafile:
+            action = self.env['account.bank.statement.import'].create({
+                'data_file': base64.b64encode(datafile.read())
+            }).import_file()
+            statements = self.env['account.bank.statement'].browse(
+                action['context']['statement_ids']
+            )
+            bank_st_record = statements[0]
+            self.assertEqual(bank_st_record.balance_start, 662.23)
+            self.assertEqual(bank_st_record.balance_end_real, 564.35)
+            statement_line = bank_st_record.line_ids[0]
+            self.assertEqual(statement_line.amount,
+                             self.transactions[0]['amount'])
+            self.assertEqual(statement_line.date,
+                             self.transactions[0]['date'])
+            self.assertEqual(statement_line.ref,
+                             self.transactions[0]['ref'])
+            self.assertEqual(statement_line.name,
+                             self.transactions[0]['name'])
+            self.assertEqual(statement_line.note,
+                             self.transactions[0]['note'])
+            statement_line = bank_st_record.line_ids[1]
+            self.assertEqual(statement_line.amount,
+                             self.transactions[1]['amount'])
+            self.assertEqual(statement_line.date,
+                             self.transactions[1]['date'])
+            self.assertEqual(statement_line.ref,
+                             self.transactions[1]['ref'])
+            self.assertEqual(statement_line.name,
+                             self.transactions[1]['name'])
+            self.assertEqual(statement_line.note,
+                             self.transactions[1]['note'])
