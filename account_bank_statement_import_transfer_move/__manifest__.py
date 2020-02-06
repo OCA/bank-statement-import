41a531a5cbb986df573591a7a216c2bb91ff4a58
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2020-02-06 16:40:46 +0200

    [ADD] account_bank_statement_import_transfer_move

---
+++ account_bank_statement_import_transfer_move/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Bank Account Transfer Line",
+    "version": "13.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/OCA/bank-statement-import",
+    "author": "Camptocamp, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["account_bank_statement_import"],
+    "data": ["view/account_journal.xml"],
+}
