1f4a47399889656f8437509e8586f942a9b2656f
Ajay Javiya <javiyaajay1987@gmail.com>
2020-04-01 18:41:41 +0530

    [MIG] Migration to 12.0: account_bank_statement_import_caisse_epargne

---
+++ account_statement_import_caisse_epargne/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    'name': 'Import CSV Bank Statement from Caisse d\'Epargne',
+    'category': 'Banking addons',
+    'version': '12.0.1.0.0',
+    'author': 'Aurélien DUMAINE, Druidoo',
+    'website': 'https://www.druidoo.io',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_bank_statement_import',
+    ],
+    'data': [
+        'views/view_account_bank_statement_import.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+}
