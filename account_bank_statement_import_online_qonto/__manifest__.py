70ed8e7e3028e5291318bbb75b0f0f600e978e05
Florent de Labarre <florent.mirieu@gmail.com>
2020-04-16 16:40:42 +0200

    [ADD] account_bank_statement_import_online_qonto

---
+++ account_bank_statement_import_online_qonto/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Online Bank Statements: Qonto.eu",
+    "version": "12.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/OCA/bank-statement-import",
+    "author": "Florent de Labarre, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["account_bank_statement_import_online"],
+    "data": [
+        "view/online_bank_statement_provider.xml"
+    ],
+}
