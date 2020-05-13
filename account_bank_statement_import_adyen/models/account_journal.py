0b793eff7a6320d7e8b40492bcc9b5749db03aa4
Martin Pishpecki <martin@vanmoof.com>
2020-05-13 17:05:05 +0200

    [MIG] 12.0 account_bank_statement_import_adyen, account_bank_statement_clearing_account

--- account_bank_statement_import_adyen/models/account_journal.py
+++ account_bank_statement_import_adyen/models/account_journal.py
@@ -1,5 +1,7 @@
-# coding: utf-8
-from openerp import fields, models
+# © 2017 Opener BV (<https://opener.amsterdam>)
+# © 2020 Vanmoof BV (<https://www.vanmoof.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
 
 
 class Journal(models.Model):
@@ -8,3 +10,9 @@ class Journal(models.Model):
     adyen_merchant_account = fields.Char(
         help=('Fill in the exact merchant account string to select this '
               'journal when importing Adyen statements'))
+
+    def _get_bank_statements_available_import_formats(self):
+        res = super(
+            Journal, self)._get_bank_statements_available_import_formats()
+        res.append('adyen')
+        return res
