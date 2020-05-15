b9ed4de5a19cb126d7c8b17febc3167a8ac14865
Alexis de Lattre <alexis.delattre@akretion.com>
2020-05-16 01:28:57 +0200

    ofx: black and others...

--- account_bank_statement_import_ofx/wizard/account_bank_statement_import.py
+++ account_bank_statement_import_ofx/wizard/account_bank_statement_import.py
@@ -1,10 +1,13 @@
-import logging
 import io
+import logging
 
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import UserError
-from odoo.addons.base_iban.models.res_partner_bank import _map_iban_template
-from odoo.addons.base_iban.models.res_partner_bank import validate_iban
+
+from odoo.addons.base_iban.models.res_partner_bank import (
+    _map_iban_template,
+    validate_iban,
+)
 
 _logger = logging.getLogger(__name__)
 
@@ -16,7 +19,7 @@ except ImportError:
 
 
 class AccountBankStatementImport(models.TransientModel):
-    _inherit = 'account.bank.statement.import'
+    _inherit = "account.bank.statement.import"
 
     def _check_journal_bank_account(self, journal, account_number):
         res = super()._check_journal_bank_account(journal, account_number)
@@ -25,11 +28,11 @@ class AccountBankStatementImport(models.TransientModel):
             e_acc_num = e_acc_num.replace(" ", "")
             validate_iban(e_acc_num)
             country_code = e_acc_num[:2].lower()
-            iban_template = _map_iban_template[country_code].replace(
-                " ", "")
+            iban_template = _map_iban_template[country_code].replace(" ", "")
             e_acc_num = "".join(
-                [c for c, t in zip(e_acc_num, iban_template) if t == "C"])
-            res = (e_acc_num == account_number)
+                [c for c, t in zip(e_acc_num, iban_template) if t == "C"]
+            )
+            res = e_acc_num == account_number
         return res
 
     @api.model
@@ -57,11 +60,11 @@ class AccountBankStatementImport(models.TransientModel):
         if transaction.memo:
             name += " : " + transaction.memo
         vals = {
-            'date': transaction.date,
-            'name': name,
-            'ref': transaction.id,
-            'amount': float(transaction.amount),
-            'unique_import_id': transaction.id,
+            "date": transaction.date,
+            "name": name,
+            "ref": transaction.id,
+            "amount": float(transaction.amount),
+            "unique_import_id": transaction.id,
         }
         return vals
 
@@ -77,18 +80,21 @@ class AccountBankStatementImport(models.TransientModel):
                 vals = self._prepare_ofx_transaction_line(transaction)
                 if vals:
                     transactions.append(vals)
-                    total_amt += vals['amount']
+                    total_amt += vals["amount"]
         except Exception as e:
-            raise UserError(_(
-                "The following problem occurred during import. "
-                "The file might not be valid.\n\n %s") % e.message)
+            raise UserError(
+                _(
+                    "The following problem occurred during import. "
+                    "The file might not be valid.\n\n %s"
+                )
+                % e.message
+            )
 
         balance = float(ofx.account.statement.balance)
         vals_bank_statement = {
-            'name': ofx.account.number,
-            'transactions': transactions,
-            'balance_start': balance - total_amt,
-            'balance_end_real': balance,
+            "name": ofx.account.number,
+            "transactions": transactions,
+            "balance_start": balance - total_amt,
+            "balance_end_real": balance,
         }
-        return ofx.account.statement.currency, ofx.account.number, [
-            vals_bank_statement]
+        return ofx.account.statement.currency, ofx.account.number, [vals_bank_statement]
