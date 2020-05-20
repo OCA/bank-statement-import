51571a03202f2635503efc480f812daf44f4911c
Lois Rilo <lois.rilo@forgeflow.com>
2020-05-20 09:29:34 +0200

    [12.0][IMP] account_bank_statement_import_txt_xlsx: * strip header names, same as Odoo does in fields if you enter   names with initial or final spaces. * Allow to select a default mapping per journal.

--- account_bank_statement_import_txt_xlsx/models/account_bank_statement_import.py
+++ account_bank_statement_import_txt_xlsx/models/account_bank_statement_import.py
@@ -10,9 +10,14 @@ _logger = logging.getLogger(__name__)
 class AccountBankStatementImport(models.TransientModel):
     _inherit = 'account.bank.statement.import'
 
+    def _get_default_mapping_id(self):
+        return self.env["account.journal"].browse(
+            self.env.context.get('journal_id')).default_sheet_mapping_id
+
     sheet_mapping_id = fields.Many2one(
         string='Sheet mapping',
         comodel_name='account.bank.statement.import.sheet.mapping',
+        default=_get_default_mapping_id,
     )
 
     @api.multi
