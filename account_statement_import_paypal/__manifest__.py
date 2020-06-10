6ef65cfebad72ed36b58c493c390cf9bbf3ab8a3
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2020-06-10 05:57:09 +0200

    Fixes account_bank_statement_import_paypal adjusting wizard after https://github.com/OCA/server-ux/pull/165 has been merged.

--- account_statement_import_paypal/__manifest__.py
+++ account_statement_import_paypal/__manifest__.py
@@ -6,7 +6,7 @@
 {
     'name': 'PayPal CSV Format Bank Statements Import',
     'summary': 'Import PayPal CSV files as Bank Statements in Odoo',
-    'version': '12.0.2.2.0',
+    'version': '12.0.2.2.1',
     'category': 'Accounting',
     'website': 'https://github.com/OCA/bank-statement-import',
     'author':
