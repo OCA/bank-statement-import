2ca07f0a35480f4578697650165a7e674a565fb3
Carlos Roca <carlos.roca@tecnativa.com>
2020-07-03 09:42:34 +0200

    [IMP] account_bank_statement_import_paypal: black, isort, prettier

--- account_statement_import_paypal/models/account_journal.py
+++ account_statement_import_paypal/models/account_journal.py
@@ -6,9 +6,9 @@ from odoo import models
 
 
 class AccountJournal(models.Model):
-    _inherit = 'account.journal'
+    _inherit = "account.journal"
 
     def _get_bank_statements_available_import_formats(self):
         res = super()._get_bank_statements_available_import_formats()
-        res.append('PayPal Reports')
+        res.append("PayPal Reports")
         return res
