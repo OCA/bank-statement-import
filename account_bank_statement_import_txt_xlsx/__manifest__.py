41006eadf0cc384f38e0a9eb71d46e923a50ad80
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-09-18 19:25:02 +0300

    [REBRAND] account_bank_statement_import_txt_xlsx: Brainbean Apps to CorporateHub

--- account_bank_statement_import_txt_xlsx/__manifest__.py
+++ account_bank_statement_import_txt_xlsx/__manifest__.py
@@ -1,14 +1,15 @@
 # Copyright 2019 ForgeFlow, S.L.
-# Copyright 2020 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Account Bank Statement Import TXT XLSX",
+    "name": "Account Bank Statement Import TXT/CSV/XLSX",
     "summary": "Import TXT/CSV or XLSX files as Bank Statements in Odoo",
     "version": "13.0.1.0.2",
     "category": "Accounting",
     "website": "https://github.com/OCA/bank-statement-import",
-    "author": "ForgeFlow, Brainbean Apps, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, CorporateHub, Odoo Community Association (OCA)",
+    "maintainers": ["alexey-pelykh"],
     "license": "AGPL-3",
     "installable": True,
     "depends": [
