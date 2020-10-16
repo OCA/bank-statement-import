fbd677d7dce6d95d39d2ed7fb9d366116c488715
Abraham Anes <abraham@studio73.es>
2020-10-16 09:05:15 +0200

    [IMP] account_bank_statement_import_qif: black, isort, prettier

--- account_bank_statement_import_qif/models/account_journal.py
+++ account_bank_statement_import_qif/models/account_journal.py
@@ -1,13 +1,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, _
+from odoo import _, models
 
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
     def _get_bank_statements_available_import_formats(self):
-        res = super(AccountJournal, self).\
-            _get_bank_statements_available_import_formats()
-        res.extend([_('.qif')])
+        res = super(
+            AccountJournal, self
+        )._get_bank_statements_available_import_formats()
+        res.extend([_(".qif")])
         return res
--- account_bank_statement_import_qif/wizards/account_bank_statement_import_qif.py
+++ account_bank_statement_import_qif/wizards/account_bank_statement_import_qif.py
@@ -6,9 +6,9 @@
 
 import dateutil.parser
 
-from odoo.tools.translate import _
 from odoo import api, models
 from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
 
 class AccountBankStatementImport(models.TransientModel):
@@ -16,22 +16,21 @@ class AccountBankStatementImport(models.TransientModel):
 
     @api.model
     def _check_qif(self, data_file):
-        return data_file.strip().startswith(b'!Type:')
+        return data_file.strip().startswith(b"!Type:")
 
     def _parse_file(self, data_file):
         if not self._check_qif(data_file):
-            return super(AccountBankStatementImport, self)._parse_file(
-                data_file)
+            return super(AccountBankStatementImport, self)._parse_file(data_file)
         try:
             file_data = data_file.decode()
-            if '\r' in file_data:
-                data_list = file_data.split('\r')
+            if "\r" in file_data:
+                data_list = file_data.split("\r")
             else:
-                data_list = file_data.split('\n')
+                data_list = file_data.split("\n")
             header = data_list[0].strip()
             header = header.split(":")[1]
         except:
-            raise UserError(_('Could not decipher the QIF file.'))
+            raise UserError(_("Could not decipher the QIF file."))
         transactions = []
         vals_line = {}
         total = 0
@@ -41,37 +40,44 @@ class AccountBankStatementImport(models.TransientModel):
                 line = line.strip()
                 if not line:
                     continue
-                if line[0] == 'D':  # date of transaction
-                    vals_line['date'] = dateutil.parser.parse(
-                        line[1:], fuzzy=True).date()
-                elif line[0] == 'T':  # Total amount
-                    total += float(line[1:].replace(',', ''))
-                    vals_line['amount'] = float(line[1:].replace(',', ''))
-                elif line[0] == 'N':  # Check number
-                    vals_line['ref'] = line[1:]
-                elif line[0] == 'P':  # Payee
-                    vals_line['name'] = (
-                        'name' in vals_line and
-                        line[1:] + ': ' + vals_line['name'] or line[1:]
+                if line[0] == "D":  # date of transaction
+                    vals_line["date"] = dateutil.parser.parse(
+                        line[1:], fuzzy=True
+                    ).date()
+                elif line[0] == "T":  # Total amount
+                    total += float(line[1:].replace(",", ""))
+                    vals_line["amount"] = float(line[1:].replace(",", ""))
+                elif line[0] == "N":  # Check number
+                    vals_line["ref"] = line[1:]
+                elif line[0] == "P":  # Payee
+                    vals_line["name"] = (
+                        "name" in vals_line
+                        and line[1:] + ": " + vals_line["name"]
+                        or line[1:]
                     )
-                elif line[0] == 'M':  # Memo
-                    vals_line['name'] = ('name' in vals_line and
-                                         vals_line['name'] + ': ' + line[1:] or
-                                         line[1:])
-                elif line[0] == '^' and vals_line:  # end of item
+                elif line[0] == "M":  # Memo
+                    vals_line["name"] = (
+                        "name" in vals_line
+                        and vals_line["name"] + ": " + line[1:]
+                        or line[1:]
+                    )
+                elif line[0] == "^" and vals_line:  # end of item
                     transactions.append(vals_line)
                     vals_line = {}
-                elif line[0] == '\n':
+                elif line[0] == "\n":
                     transactions = []
                 else:
                     pass
         else:
-            raise UserError(_('This file is either not a bank statement or is '
-                              'not correctly formed.'))
-        vals_bank_statement.update({
-            'balance_end_real': total,
-            'transactions': transactions
-        })
+            raise UserError(
+                _(
+                    "This file is either not a bank statement or is "
+                    "not correctly formed."
+                )
+            )
+        vals_bank_statement.update(
+            {"balance_end_real": total, "transactions": transactions}
+        )
         return None, None, [vals_bank_statement]
 
     def _complete_stmts_vals(self, stmt_vals, journal_id, account_number):
@@ -82,12 +88,12 @@ class AccountBankStatementImport(models.TransientModel):
         # Since QIF doesn't provide account numbers (normal behaviour is to
         # provide 'account_number', which the generic module uses to find
         # the partner), we have to find res.partner through the name
-        partner_obj = self.env['res.partner']
+        partner_obj = self.env["res.partner"]
         for statement in res:
-            for line_vals in statement['transactions']:
-                if not line_vals.get('partner_id') and line_vals.get('name'):
+            for line_vals in statement["transactions"]:
+                if not line_vals.get("partner_id") and line_vals.get("name"):
                     partner = partner_obj.search(
-                        [('name', 'ilike', line_vals['name'])], limit=1,
+                        [("name", "ilike", line_vals["name"])], limit=1,
                     )
-                    line_vals['partner_id'] = partner.id
+                    line_vals["partner_id"] = partner.id
         return res
