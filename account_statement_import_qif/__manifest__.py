3af7a2dc562c0049d063929143a4516b790b50b2
Abraham Anes <abraham@studio73.es>
2020-10-16 09:31:45 +0200

    [MIG] account_bank_statement_import_qif: Migration to 13.0
    TT26069
    
    [UPD] Update account_bank_statement_import_qif.pot
    
    [UPD] README.rst
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: bank-statement-import-13.0/bank-statement-import-13.0-account_bank_statement_import_qif
    Translate-URL: https://translation.odoo-community.org/projects/bank-statement-import-13-0/bank-statement-import-13-0-account_bank_statement_import_qif/

--- account_statement_import_qif/__manifest__.py
+++ account_statement_import_qif/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Import QIF Bank Statements",
     "category": "Accounting",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "OpenERP SA," "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/bank-statement-import",
     "depends": ["account_bank_statement_import"],
