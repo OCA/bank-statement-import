d33d86bd302027ac53afc8174dd067bdabc7fa3f
João Marques <joao.marques@tecnativa.com>
2020-11-02 12:16:03 +0000

    [IMP] account_bank_statement_import_move_line: black, isort, prettier

--- account_bank_statement_import_move_line/__manifest__.py
+++ account_bank_statement_import_move_line/__manifest__.py
@@ -2,23 +2,20 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Bank statement import move lines',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting',
-    'summary': 'Import journal items into bank statement',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'maintainers': ['pedrobaeza'],
-    'website': 'https://github.com/OCA/bank-statement-import',
-    'depends': [
-        'account',
+    "name": "Bank statement import move lines",
+    "version": "12.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Import journal items into bank statement",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "maintainers": ["pedrobaeza"],
+    "website": "https://github.com/OCA/bank-statement-import",
+    "depends": ["account",],
+    "data": [
+        "wizards/account_statement_line_create_view.xml",
+        "views/account_bank_statement_view.xml",
     ],
-    'data': [
-        'wizards/account_statement_line_create_view.xml',
-        'views/account_bank_statement_view.xml',
-    ],
-    'license': 'AGPL-3',
-    'development_status': 'Production/Stable',
-    'installable': True,
-    'auto_install': False,
+    "license": "AGPL-3",
+    "development_status": "Production/Stable",
+    "installable": True,
+    "auto_install": False,
 }
