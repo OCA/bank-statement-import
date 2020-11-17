34b22800324997ef34258e7a98be59d9c122b2eb
Alexis de Lattre <alexis.delattre@akretion.com>
2020-11-18 00:22:27 +0100

    [MIG] account_bank_statement_import to v14 > account_statement_import
    Module renamed to account_statement_import to avoid conflit with Odoo enterprise
    Add support for multi-account statement files
    Integrate the feature provided by the module account_bank_statement_import_save_file
    Improve error messages
    Remove dead or annoying features
    Improve code !

--- account_statement_import/wizard/setup_wizards.py
+++ account_statement_import/wizard/setup_wizards.py
@@ -1,15 +1,15 @@
 # Copyright 2004-2020 Odoo S.A.
 # Licence LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
-from odoo import api, fields, models
+from odoo import models
 
 
-class SetupBarBankConfigWizard(models.TransientModel):
+class AccountSetupBankManualConfig(models.TransientModel):
     _inherit = "account.setup.bank.manual.config"
 
     def validate(self):
         """Default the bank statement source of new bank journals as 'file_import'"""
-        super(SetupBarBankConfigWizard, self).validate()
+        super().validate()
         if (
             self.num_journals_without_account == 0
             or self.linked_journal_id.bank_statements_source == "undefined"
