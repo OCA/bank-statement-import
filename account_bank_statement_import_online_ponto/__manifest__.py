1b5d30e042edcb25bf1d9ea32274a0deda9237f4
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-12-12 18:21:14 +0100

    [FIX+IMP] account_bank_statement_import_online_ponto: Adjustments for better use
    * FIX: Returned dates are in UTC, so they were classified incorrectly in
      the bad statement.
    * FIX: Information in ref was the most important one, being the other
      mostly '/'
    * IMP: Preserve all possible information from the source, including
      counterpart information.
    * IMP: Fill Odoo statement line fields for account number and partner
      name for better support.

--- account_bank_statement_import_online_ponto/__manifest__.py
+++ account_bank_statement_import_online_ponto/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Online Bank Statements: MyPonto.com",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "category": "Account",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": "Florent de Labarre, Odoo Community Association (OCA)",
