d126f8192afebedd977c1a4be22c0315d82b821e
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-01-07 09:10:51 +0100

    [IMP] account_bank_statement_import: black, isort

--- account_statement_import/__manifest__.py
+++ account_statement_import/__manifest__.py
@@ -1,28 +1,27 @@
-# -*- encoding: utf-8 -*-
 # Copyright 2004-2020 Odoo S.A.
 # Licence LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
 {
-    'name': 'Account Bank Statement Import',
-    'category': 'Accounting/Accounting',
-    'version': '13.0.1.0.0',
-    'license': 'LGPL-3',
-    'depends': ['account'],
-    'description': """Generic Wizard to Import Bank Statements.
+    "name": "Account Bank Statement Import",
+    "category": "Accounting/Accounting",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "description": """Generic Wizard to Import Bank Statements.
 
 (This module does not include any type of import format.)
 
 OFX and QIF imports are available in Enterprise version.""",
-    'author': 'Odoo SA',
-    'data': [
-        'account_bank_statement_import_view.xml',
-        'account_import_tip_data.xml',
-        'wizard/journal_creation.xml',
-        'views/account_bank_statement_import_templates.xml',
+    "author": "Odoo SA",
+    "data": [
+        "account_bank_statement_import_view.xml",
+        "account_import_tip_data.xml",
+        "wizard/journal_creation.xml",
+        "views/account_bank_statement_import_templates.xml",
     ],
-    'demo': [
-        'demo/partner_bank.xml',
+    "demo": [
+        "demo/partner_bank.xml",
     ],
-    'installable': True,
-    'auto_install': True,
+    "installable": True,
+    "auto_install": True,
 }
