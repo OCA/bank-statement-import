5a4c033c41cd4977ddba1f3026a015760868cc47
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2021-01-11 15:35:19 +0200

    [MIG][14.0] account_statement_import_camt_oca

---
+++ account_statement_import_camt/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2013-2017 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CAMT Format Bank Statements Import",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/bank-statement-import",
+    "category": "Banking addons",
+    "depends": ["account_statement_import"],
+    "data": ["views/account_bank_statement_import.xml"],
+}
