558d9a33ddff55921acd204cd803e9adab830cec
Ronald Portier <ronald@therp.nl>
2021-02-17 16:35:49 +0100

    [MIG] account_bank_statement_clearing_account Migrate to 13.0

--- account_bank_statement_clearing_account/__manifest__.py
+++ account_bank_statement_clearing_account/__manifest__.py
@@ -1,13 +1,13 @@
-# © 2017 Opener BV (<https://opener.amsterdam>)
-# © 2020 Vanmoof BV (<https://www.vanmoof.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2017 Opener BV (<https://opener.amsterdam>)
+# Copyright 2020 Vanmoof BV (<https://www.vanmoof.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Reconcile entries from pseudo bank statements",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Opener B.V., Vanmoof BV, Odoo Community Association (OCA)",
     "category": "Banking addons",
-    "website": "https://opener.am",
+    "website": "https://github.com/oca/bank-statement-import",
     "license": "AGPL-3",
-    "depends": ["account_cancel",],
+    "depends": ["account"],
     "installable": True,
 }
