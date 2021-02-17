c385ec0c787b1018bbea453478df5c876636e14a
Ronald Portier <ronald@therp.nl>
2021-02-17 18:08:42 +0100

    [ADD] account_bank_statement_import_online_adyen

---
+++ account_bank_statement_import_online_adyen/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 - Therp BV <https://therp.nl>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Online Bank Statements: Adyen payment report",
+    "version": "13.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/OCA/bank-statement-import",
+    "author": "Ronald Portier (Therp BV), Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_bank_statement_import_adyen",
+        "account_bank_statement_import_online",
+    ],
+    "data": ["views/online_bank_statement_provider.xml"],
+}
