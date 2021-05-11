5b9b26c1432be3a0ed0d4137da6f89b8d6bacb5b
Alexey Pelykh <alexey.pelykh@gmail.com>
2021-05-11 21:57:29 +0200

    [MIG] account_bank_statement_import_online_transferwise: Migration to 13.0

--- account_bank_statement_import_online_transferwise/__manifest__.py
+++ account_bank_statement_import_online_transferwise/__manifest__.py
@@ -4,8 +4,8 @@
 
 {
     "name": "Online Bank Statements: Wise.com (TransferWise.com)",
-    "version": "12.0.1.0.3",
-    "author": "CorporateHub, " "Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "CorporateHub, Odoo Community Association (OCA)",
     "maintainers": ["alexey-pelykh"],
     "website": "https://github.com/OCA/bank-statement-import/",
     "license": "AGPL-3",
