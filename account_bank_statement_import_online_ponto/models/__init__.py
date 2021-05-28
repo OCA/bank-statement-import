5d62d497d92298ecf9687892d2f0e5e7543ab5a2
Ronald Portier <ronald@therp.nl>
2021-05-28 14:57:23 +0200

    [IMP] Adapt account_bank_statement_import_online to 14.0
    Also Rename account_bank_statement_import_online => account_statement_import_online

--- account_bank_statement_import_online_ponto/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2020 Florent de Labarre
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import online_bank_statement_provider_ponto
--- account_bank_statement_import_online_ponto/readme/CONTRIBUTORS.rst
+++
@@ -1,5 +0,0 @@
-* Florent de Labarre
-* `Tecnativa <https://www.tecnativa.com>`__:
-
-  * Pedro M. Baeza
-  * João Marques
