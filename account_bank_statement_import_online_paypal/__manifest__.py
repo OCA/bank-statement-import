15c36bd914f99088414dd49cbfa59dc6335a0693
Alexey Pelykh <alexey.pelykh@gmail.com>
2021-09-23 09:10:02 +0200

    [IMP] account_bank_statement_import_online_paypal: black, isort, prettier

--- account_bank_statement_import_online_paypal/__manifest__.py
+++ account_bank_statement_import_online_paypal/__manifest__.py
@@ -3,21 +3,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Online Bank Statements: PayPal.com',
-    'version': '12.0.1.1.0',
-    'author':
-        'CorporateHub, '
-        'Odoo Community Association (OCA)',
-    'maintainers': ['alexey-pelykh'],
-    'website': 'https://github.com/OCA/bank-statement-import/',
-    'license': 'AGPL-3',
-    'category': 'Accounting',
-    'summary': 'Online bank statements for PayPal.com',
-    'depends': [
-        'account_bank_statement_import_online',
-    ],
-    'data': [
-        'views/online_bank_statement_provider.xml',
-    ],
-    'installable': True,
+    "name": "Online Bank Statements: PayPal.com",
+    "version": "12.0.1.1.0",
+    "author": "CorporateHub, " "Odoo Community Association (OCA)",
+    "maintainers": ["alexey-pelykh"],
+    "website": "https://github.com/OCA/bank-statement-import/",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "summary": "Online bank statements for PayPal.com",
+    "depends": ["account_bank_statement_import_online",],
+    "data": ["views/online_bank_statement_provider.xml",],
+    "installable": True,
 }
