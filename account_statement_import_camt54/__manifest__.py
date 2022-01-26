a3797c9cf93e8d39b9b41c94bbd05e7bc333a8ad
Maksym Yankin <yankinmk@gmail.com>
2022-01-26 15:34:13 +0200

    [MIG] account_statement_import_camt54: Migration to 15.0

--- account_statement_import_camt54/__manifest__.py
+++ account_statement_import_camt54/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Bank Account Camt54 Import",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": "camptocamp, " "Odoo Community Association (OCA)",
