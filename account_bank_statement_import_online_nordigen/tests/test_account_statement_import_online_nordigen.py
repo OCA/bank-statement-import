13d445d3990186d989d450657ad4584a9fdd791c
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-02-03 08:45:29 -0500

    [14.0][ADD] account_bank_statement_import_online_nordigen

---
+++ account_bank_statement_import_online_nordigen/tests/test_account_statement_import_online_nordigen.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests import common
+
+
+class TestAccountBankAccountStatementImportOnlineNordigen(common.TransactionCase):
+    def setUp(self):
+        super().setUp()
+
+    def test_balance_start(self):
+        return
+
+    def test_nordigen(self):
+        return
