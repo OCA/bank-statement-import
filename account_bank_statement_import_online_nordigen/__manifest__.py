2cd3ac528fabfbd42db3e1ccce3c46a8fb900095
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-02-21 13:21:13 -0500

    [FIX] version

--- account_bank_statement_import_online_nordigen/__manifest__.py
+++ account_bank_statement_import_online_nordigen/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Online Bank Statements: Nordigen",
-    "version": "13.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
