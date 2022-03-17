88b8b572a8d801d1cdc7a57d05ffc0537e325c2a
Ronald Portier <ronald@therp.nl>
2022-03-17 15:58:25 +0100

    [IMP] *_online_adyen: Directly parse retrieved adyen file
    Prevent other statement import modules from processing the file. Without this for instance the
    Enterprise account_bank_statement_import_csv module will process a retrieved cv file. In addition this
    is more efficient as it saves a decoding step.

--- account_bank_statement_import_online_adyen/tests/online_bank_statement_provider_dummy.py
+++ account_bank_statement_import_online_adyen/tests/online_bank_statement_provider_dummy.py
@@ -1,12 +1,13 @@
-# Copyright 2021 Therp BV <https://therp.nl>.
+# Copyright 2021-2022 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-import base64
-
+"""Dummy provider gets files from file-system, instead of from api."""
 from odoo import models
 from odoo.modules.module import get_module_resource
 
 
 class OnlineBankStatementProviderDummy(models.Model):
+    """Dummy provider gets files from file-system, instead of from api."""
+
     _inherit = "online.bank.statement.provider"
 
     def _adyen_get_settlement_details_file(self):
@@ -20,5 +21,4 @@ class OnlineBankStatementProviderDummy(models.Model):
         )
         with open(testfile, "rb") as datafile:
             data_file = datafile.read()
-            data_file = base64.b64encode(data_file)
         return data_file, filename
