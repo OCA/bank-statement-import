a5ec64a3270beeab9062cac949fbe08eb096573a
Henrik Norlin <henrik.norl@gmail.com>
2022-04-03 15:06:07 +0200

    [IMP] account_statement_import_txt_xlsx: wizard: 2 amount columns, for IN and OUT
    [IMP] account_statement_import_txt_xlsx: wizard: amount2_reverse boolean field, with tests

--- account_statement_import_txt_xlsx/models/account_statement_import_sheet_mapping.py
+++ account_statement_import_txt_xlsx/models/account_statement_import_sheet_mapping.py
@@ -66,6 +66,14 @@ class AccountStatementImportSheetMapping(models.Model):
         required=True,
         help="Amount of transaction in journal's currency",
     )
+    amount2_column = fields.Char(
+        string="Amount2 column",
+        help="Some statements have two amount columns, for IN and OUT",
+    )
+    amount2_reverse = fields.Boolean(
+        string="Amount2 reverse +/-",
+        help="If there are positive numbers for money going OUT, reverse +/-",
+    )
     balance_column = fields.Char(
         help="Balance after transaction in journal's currency",
     )
