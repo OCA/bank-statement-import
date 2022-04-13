f38e249e70b068ba3a3061fd4f2632762879c05a
manu <manuel.regidor@sygel.es>
2022-04-13 14:03:04 +0200

    [MIG]account_statement_import_online_ponto: Migration to 15.0
    [UPD] Update account_statement_import_online_ponto.pot

--- account_statement_import_online_ponto/__manifest__.py
+++ account_statement_import_online_ponto/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Online Bank Statements: MyPonto.com",
-    "version": "14.0.2.0.0",
+    "version": "15.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": "Florent de Labarre, Therp BV, Odoo Community Association (OCA)",
