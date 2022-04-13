459276c2a0f0f7833edb239f9dace48b3d96f177
manu <manuel.regidor@sygel.es>
2022-04-13 13:09:36 +0200

    [MIG]account_statement_import_online: Migration to 15.0

--- account_statement_import_online/__manifest__.py
+++ account_statement_import_online/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Online Bank Statements",
-    "version": "14.0.3.0.0",
+    "version": "15.0.1.0.0",
     "author": "CorporateHub, Odoo Community Association (OCA)",
     "maintainers": ["alexey-pelykh"],
     "website": "https://github.com/OCA/bank-statement-import",
