78fa953e350951dc7aaff2e1b3dd0c9adb9ba48e
Karolin Schlegel <karolin.schlegel@googlemail.com>
2022-06-24 11:59:25 +0200

    Add python-fints as requirement

--- account_statement_import_online_fints/__manifest__.py
+++ account_statement_import_online_fints/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Online Bank Statements: FinTS",
-    "version": "14.0.0.0.0",
+    "version": "14.0.1.0.0",
     "category": "Account",
     "author": "Karolin Schlegel, Odoo Community Association (OCA)",
     "license": "AGPL-3",
