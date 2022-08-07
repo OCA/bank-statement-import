dfb14ac7def8de7dd65c9950d1a0436a4093310d
Alexis de Lattre <alexis.delattre@akretion.com>
2022-08-08 00:05:18 +0200

    Add module account_statement_import_base
    The 2 modules account_statement_import_online and
    account_statement_import depend on account_statement_import_base (and
    not on each other) and share common code, in particular a hook to update
    the statement line. So we can now have reconciliation modules that use
    this hook and therefore work both on file import and online import. More
    details on https://github.com/OCA/bank-statement-import/issues/481.
    
    Improve bank statement line form view and journal form view.

--- account_statement_import_online/models/__init__.py
+++ account_statement_import_online/models/__init__.py
@@ -1,5 +1,4 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import account_journal
-from . import account_bank_statement_line
 from . import online_bank_statement_provider
