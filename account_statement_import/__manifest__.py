0f37ec001992f8fe2a88412cf8c2aa3ff4ab75a0
Alexis de Lattre <alexis.delattre@akretion.com>
2022-08-08 00:05:18 +0200

    Add module account_statement_import_base
    The 2 modules account_statement_import_online and
    account_statement_import depend on account_statement_import_base (and
    not on each other) and share common code, in particular a hook to update
    the statement line. So we can now have reconciliation modules that use
    this hook and therefore work both on file import and online import. More
    details on https://github.com/OCA/bank-statement-import/issues/481.
    
    Improve bank statement line form view and journal form view.

--- account_statement_import/__manifest__.py
+++ account_statement_import/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Accounting",
     "version": "14.0.2.1.0",
     "license": "LGPL-3",
-    "depends": ["account"],
+    "depends": ["account_statement_import_base"],
     "author": "Odoo SA, Akretion, Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
     "development_status": "Mature",
@@ -17,7 +17,6 @@
         "security/ir.model.access.csv",
         "wizard/account_statement_import_view.xml",
         "views/account_journal.xml",
-        "views/account_bank_statement_line.xml",
     ],
     "demo": [
         "demo/partner_bank.xml",
