e2a39d9c24f48fe21e9974422a0c02e6c87f7a2f
Jordi Ballester <jordi.ballester@forgeflow.com>
2022-09-15 19:18:43 +0200

    [MIG] account_bank_statement_import_online_nordigen: Migration to 15.0

--- account_bank_statement_import_online_nordigen/__manifest__.py
+++ account_bank_statement_import_online_nordigen/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Online Bank Statements: Nordigen",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
@@ -15,10 +15,21 @@
         "web",
         "web_notify",
     ],
-    "data": ["view/assets.xml", "view/online_bank_statement_provider.xml"],
-    "qweb": [
-        "static/src/xml/*.xml",
-    ],
+    "data": ["view/online_bank_statement_provider.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "account_bank_statement_import_online_nordigen/static/src/"
+            "lib/nordigen-ui/selector.css",
+            "account_bank_statement_import_online_nordigen/static/src/"
+            "lib/nordigen-ui/selector.js",
+            "account_bank_statement_import_online_nordigen/static/src/"
+            "js/select_bank_widget.js",
+        ],
+        "web.assets_qweb": [
+            "account_bank_statement_import_online_nordigen/static/src/xml"
+            "/select_bank_widget.xml"
+        ],
+    },
     "external_dependencies": {
         "python": [],
     },
