9c931f38a5a3f393eaeff673cc24ead28222dd62
ntsirintanis <ntsirintanis@therp.nl>
2022-09-16 15:21:38 +0200

    [BACKPORT] account_bank_statement_import_guess_partner

--- account_bank_statement_import_guess_partner/__manifest__.py
+++ account_bank_statement_import_guess_partner/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Guess partner on import of statement lines",
-    "version": "13.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Invoicing Management",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": "Therp BV, Odoo Community Association (OCA)",
