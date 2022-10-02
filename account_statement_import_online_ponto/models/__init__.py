ae3fa713e42938133c804872f511e9117e85b775
Ronald Portier (Therp BV) <ronald@therp.nl>
2022-10-02 13:15:38 +0200

    [IMP] *_online_ponto: forward port 12.0 improvements
    Separate retrieval of data from ponto (buffer data) and
    creation of statements.

--- account_statement_import_online_ponto/models/__init__.py
+++ account_statement_import_online_ponto/models/__init__.py
@@ -1,3 +1,5 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
+from . import ponto_buffer
+from . import ponto_buffer_line
+from . import ponto_interface
 from . import online_bank_statement_provider_ponto
