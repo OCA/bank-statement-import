73261edb9086423ef825e8f2ca5779cd38c3da63
Enric Tobella <etobella@creublanca.es>
2022-11-17 16:40:16 +0100

    [IMP] account_statment_import: Add button for importing on top

---
+++ account_statement_import/static/src/js/account_dashboard_kanban.js
@@ -0,0 +1,21 @@
+odoo.define("account_statement_import.dashboard.kanban", function (require) {
+    "use strict";
+    var viewRegistry = require("web.view_registry");
+
+    var AccountDashboardView = viewRegistry.get("account_dashboard_kanban");
+    var AccountDashboardController = AccountDashboardView.prototype.config.Controller;
+    AccountDashboardController.include({
+        buttons_template: "AccountDashboardView.buttons",
+        // We are reusing the create button
+        _onButtonNew: function (ev) {
+            ev.stopPropagation();
+            return this.trigger_up("do_action", {
+                action: "account_statement_import.account_statement_import_action",
+            });
+        },
+    });
+    return {
+        AccountDashboardView: AccountDashboardView,
+        AccountDashboardController: AccountDashboardController,
+    };
+});
