c076e0da8546525b96926e5128d505e1277c3418
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-01-24 14:31:22 +0530

    [ADD] account_bank_statement_import_online_ofx

---
+++ account_statement_import_online_ofx/wizards/online_bank_statement_pull_wizard.py
@@ -0,0 +1,32 @@
+# Copyright 2023 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class OnlineBankStatementPullWizard(models.TransientModel):
+    _inherit = "online.bank.statement.pull.wizard"
+
+    ofx_institution_ids = fields.Many2many(
+        string="OFX Institutions",
+        comodel_name="ofx.institution.line",
+        column1="wizard_id",
+        column2="institution_line_id",
+        relation="ofx_institution_line_pull_wizard_rel",
+    )
+    is_ofx_provider = fields.Boolean()
+
+    @api.onchange("provider_ids")
+    def onchange_provider_ids(self):
+        for rec in self:
+            rec.is_ofx_provider = False
+            for provider in rec.provider_ids:
+                if provider.service == "OFX":
+                    rec.is_ofx_provider = True
+                    break
+
+    def action_pull(self):
+        return super(
+            OnlineBankStatementPullWizard,
+            self.with_context(ofx_institution_ids=self.ofx_institution_ids.ids),
+        ).action_pull()
