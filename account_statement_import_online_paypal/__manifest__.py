f05dd1ba61a55be1cc4cea39de54a21efce9457c
Ken Woychesko <ken@airmdb.com>
2023-05-03 17:06:06 -0400

    [FIX] account_statement_import_online_paypal: force observance of "Allow Empty Statements" provider setting
    Empty statements should only be created if "Allow Empty Statements" is checked. This commit adds a check for the state of the allow_empty_statements field.

--- account_statement_import_online_paypal/__manifest__.py
+++ account_statement_import_online_paypal/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Online Bank Statements: PayPal.com",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.1.1",
     "author": "CorporateHub, Odoo Community Association (OCA)",
     "maintainers": ["alexey-pelykh"],
     "website": "https://github.com/OCA/bank-statement-import",
