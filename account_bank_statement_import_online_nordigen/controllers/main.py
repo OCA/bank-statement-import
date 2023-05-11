6e8f7f357e272a6d1aa459c0dd8362558b259634
Entrepreneur AJ <contact@entrepreneuraj.com>
2023-05-11 19:54:32 +0100

    Commit the hard work already created by Jordi with PR https://github.com/OCA/bank-statement-import/pull/496 Remove the sandbox institution id that prevented real open banking operation.

---
+++ account_bank_statement_import_online_nordigen/controllers/main.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+import logging
+
+import werkzeug
+from werkzeug.urls import url_encode
+
+from odoo import http
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class NordigenController(http.Controller):
+    @http.route("/nordigen/response", type="http", auth="public", csrf=False)
+    def nordigen_response(self, **post):
+        _logger.debug(post)
+        statement_provider = request.env["online.bank.statement.provider"].sudo()
+        current_provider = statement_provider.search(
+            [("nordigen_last_requisition_ref", "=", post.get("ref", False))]
+        )
+        action_id = request.env.ref(
+            "account_statement_import_online.online_bank_statement_provider_action"
+        ).id
+        params = {
+            "action": action_id,
+            "view_type": "list",
+        }
+        url = "/web#" + url_encode(params)
+        if current_provider:
+            current_provider.update_nordigen_request()
+            url = current_provider._get_nordigen_url_action()
+        return werkzeug.utils.redirect(url, 303)
---
+++ account_bank_statement_import_online_nordigen/tests/test_account_statement_import_online_nordigen.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests import common
+
+SAMPLE_ACCOUNT_NUMBER_1 = "GL3343697694912188"
+SAMPLE_ACCOUNT_NUMBER_2 = "GL0865354374424724"
+
+
+class TestAccountBankAccountStatementImportOnlineNordigen(common.TransactionCase):
+    def setUp(self):
+        super().setUp()
+
+    def test_balance_start(self):
+        return
+
+    def test_nordigen(self):
+        return
