c118433ca8cb7c74ea8981a6e9dfe15611a49c73
Emanuel Cino <ecino@compassion.ch>
2023-06-01 11:41:07 +0200

    [MIG] account_statement_import_online_wise: Migration to 14.0

---
+++ account_statement_import_online_wise/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020-2021 CorporateHub (https://corporatehub.eu)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Online Bank Statements: Wise.com (TransferWise.com)",
+    "version": "14.0.1.0.0",
+    "author": "CorporateHub, Odoo Community Association (OCA)",
+    "maintainers": ["alexey-pelykh"],
+    "website": "https://github.com/OCA/bank-statement-import",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "summary": "Online bank statements for Wise.com (TransferWise.com)",
+    "depends": ["account_statement_import_online", "web_widget_dropdown_dynamic"],
+    "external_dependencies": {"python": ["cryptography"]},
+    "data": ["views/online_bank_statement_provider.xml"],
+    "installable": True,
+}
