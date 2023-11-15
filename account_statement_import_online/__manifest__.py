0f1c440a242b627347ce06541e4a3b8c553796d9
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-11-15 08:16:29 +0100

    [FIX] account_statement_import_online: Fix es.po key
    ****po-python-parse-format****
    account_statement_import_online/i18n/es.po:136 Translation string couldn't be parsed correctly using str.format KeyError('excepction') - [po-python-parse-format]

--- account_statement_import_online/__manifest__.py
+++ account_statement_import_online/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Online Bank Statements",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "author": "CorporateHub, Odoo Community Association (OCA)",
     "maintainers": ["alexey-pelykh"],
     "website": "https://github.com/OCA/bank-statement-import",
