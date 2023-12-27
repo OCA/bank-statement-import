51f4ee7010d3cf556b814a937ea77f85cb8086e0
Rocío Vega <rov@adhoc.com.ar>
2023-12-27 10:11:01 -0300

    [16.0][FIX] models/account_statement_import_file: Improve the identification of the OCA file import method

--- account_statement_import_file/models/account_journal.py
+++ account_statement_import_file/models/account_journal.py
@@ -28,7 +28,15 @@ class AccountJournal(models.Model):
             formats_list.sort()
             import_formats_str = ", ".join(formats_list)
             rslt.insert(
-                0, ("file_import_oca", _("Import") + "(" + import_formats_str + ")")
+                0,
+                (
+                    "file_import_oca",
+                    _("Import")
+                    + " Statement Sheet Mapping"
+                    + "("
+                    + import_formats_str
+                    + ")",
+                ),
             )
         return rslt
 
