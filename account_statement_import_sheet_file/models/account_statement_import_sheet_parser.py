3566322c09c580d759ab1c816a80ef9a6842e1ad
Rocío Vega <rov@adhoc.com.ar>
2023-12-27 10:54:17 -0300

    [MIG] account_statement_import_sheet_file: Migration to 17.0

--- account_statement_import_sheet_file/models/account_statement_import_sheet_parser.py
+++ account_statement_import_sheet_file/models/account_statement_import_sheet_parser.py
@@ -256,18 +256,18 @@ class AccountStatementImportSheetParser(models.TransientModel):
                 else currency_code
             )
 
-            def _decimal(column_name):
+            def _decimal(column_name, values):
                 if columns[column_name]:
                     return self._parse_decimal(
                         self._get_values_from_column(values, columns, column_name),
                         mapping,
                     )
 
-            amount = _decimal("amount_column")
+            amount = _decimal("amount_column", values)
             if not amount:
-                amount = abs(_decimal("amount_debit_column") or 0)
+                amount = abs(_decimal("amount_debit_column", values) or 0)
             if not amount:
-                amount = -abs(_decimal("amount_credit_column") or 0)
+                amount = -abs(_decimal("amount_credit_column", values) or 0)
 
             balance = (
                 self._get_values_from_column(values, columns, "balance_column")
@@ -354,6 +354,13 @@ class AccountStatementImportSheetParser(models.TransientModel):
                 amount = abs(amount)
                 if debit_credit == mapping.debit_value:
                     amount = -amount
+
+            if debit_column and credit_column:
+                debit_amount = self._parse_decimal(debit_column, mapping)
+                debit_amount = abs(debit_amount)
+                credit_amount = self._parse_decimal(credit_column, mapping)
+                credit_amount = abs(credit_amount)
+                amount = -(credit_amount - debit_amount)
 
             if original_amount:
                 original_amount = math.copysign(
