b635b3be65f6b8ff2eefe7e0ae20d9d33bc589fd
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-01-30 11:33:00 +0100

    [IMP] account_statement_import_online: Add debug wizard
    It's useful to debug RAW data received from your online sources. With
    this improvement, you can do it easily from the manual pull wizard. It's
    only available for users in debug mode (or with technical features).
    
    TT47238

--- account_statement_import_online/__manifest__.py
+++ account_statement_import_online/__manifest__.py
@@ -19,6 +19,7 @@
         "data/account_statement_import_online.xml",
         "security/ir.model.access.csv",
         "security/online_bank_statement_provider.xml",
+        "wizards/online_bank_statement_pull_debug.xml",
         "wizards/online_bank_statement_pull_wizard.xml",
         "views/actions.xml",
         "views/account_journal.xml",
