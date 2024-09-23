b54d9ce23481b4c4a4a4d9c2686f87435604c40a
Florian da Costa <florian.dacosta@akretion.com>
2024-09-23 08:56:29 +0200

    [FIX] account_statement_import_online_qonto : Remove verify = false in calls to qonto

--- account_statement_import_online_qonto/__manifest__.py
+++ account_statement_import_online_qonto/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Online Bank Statements: Qonto",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "category": "Account",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": "Florent de Labarre, Odoo Community Association (OCA)",
