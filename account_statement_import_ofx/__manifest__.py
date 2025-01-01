f8c2dae916ac7666532e2cff34ffbf25f93975bd
Alexis de Lattre <alexis.delattre@akretion.com>
2025-01-01 11:00:50 +0100

    [MIG] account_statement_import_ofx to v18

--- account_statement_import_ofx/__manifest__.py
+++ account_statement_import_ofx/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Import OFX Bank Statement",
     "category": "Banking addons",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Odoo SA,"
     "Akretion,"
