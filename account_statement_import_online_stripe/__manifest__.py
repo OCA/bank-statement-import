e919c048146592f95e6f28d829aaf54ebbb55872
juancarlosonate-tecnativa <juancarlos.onate@tecnativa.com>
2025-03-03 18:01:26 +0100

    [ADD] account_statement_import_online_stripe
    TT54821

---
+++ account_statement_import_online_stripe/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Online Bank Statements: Stripe",
+    "version": "17.0.1.0.0",
+    "author": "Akiles, Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["juancarlosonate-tecnativa"],
+    "website": "https://github.com/OCA/bank-statement-import",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "summary": "Online bank statements for Stripe",
+    "depends": ["account_statement_import_online"],
+    "data": ["views/online_bank_statement_provider.xml"],
+    "installable": True,
+}
