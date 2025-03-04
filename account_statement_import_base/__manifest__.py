effbdbcd09c0b77808576f539156272f079d3468
Yasser Abdelkarim <82792332+Yassir-17@users.noreply.github.com>
2025-03-04 22:53:25 +0400

    Update __manifest__.py

--- account_statement_import_base/__manifest__.py
+++ account_statement_import_base/__manifest__.py
@@ -1,4 +1,4 @@
-# Test Runboat11111111qqq111
+# Test Runboat1q1111111qqq111
 # Copyright 2022 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # Licence LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
