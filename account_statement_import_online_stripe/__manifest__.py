445469cbf6546629c7e17deeb0b242c00ce5fff1
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-08-01 08:47:43 +0200

    [FIX] account_statement_import_online_stripe: Hide Stripe configuration if different provider

--- account_statement_import_online_stripe/__manifest__.py
+++ account_statement_import_online_stripe/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Online Bank Statements: Stripe",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "author": "Akiles, Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["juancarlosonate-tecnativa"],
     "website": "https://github.com/OCA/bank-statement-import",
