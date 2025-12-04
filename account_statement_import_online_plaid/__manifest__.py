e6e2369973790a434f29391a34261d0b6e5728eb
Antonio David Ruban <antoniodavid8@gmail.com>
2025-12-04 14:56:04 -0500

    [MIG] account_statement_import_online_plaid: Migration to 18.0

--- account_statement_import_online_plaid/__manifest__.py
+++ account_statement_import_online_plaid/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Online Bank Statements: plaid.com",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": "Binhex, Odoo Community Association (OCA)",
