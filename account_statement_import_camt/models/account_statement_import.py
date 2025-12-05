50568274d9e20c791eb06ec17bf5fc8537a0694c
Quentin Groulard <quentin.groulard@acsone.eu>
2025-12-05 16:31:41 +0100

    [IMP] account_statement_import_camt: handle "merged" camt files
    Some banks (typically BNP France) would merge several camt files into one.

--- account_statement_import_camt/models/account_statement_import.py
+++ account_statement_import_camt/models/account_statement_import.py
@@ -4,7 +4,7 @@ import logging
 import zipfile
 from io import BytesIO
 
-from odoo import models
+from odoo import api, models
 
 _logger = logging.getLogger(__name__)
 
@@ -12,12 +12,27 @@ _logger = logging.getLogger(__name__)
 class AccountStatementImport(models.TransientModel):
     _inherit = "account.statement.import"
 
+    @api.model
+    def _xml_split_file(self, data_file):
+        """BNP France is known to merge xml files"""
+        if not data_file.startswith(b"<?xml"):
+            return [data_file]
+        data_file_elements = []
+        all_files = data_file.split(b"<?xml")
+        for file in all_files:
+            if file:
+                data_file_elements.append(b"<?xml" + file)
+        return data_file_elements
+
     def _parse_file(self, data_file):
         """Parse a CAMT053 XML file."""
         if not zipfile.is_zipfile(BytesIO(data_file)):
             parser = self.env["account.statement.import.camt.parser"]
             _logger.debug("Try parsing with camt.")
-            return parser.parse(data_file)
+            result = []
+            for data_file_element in self._xml_split_file(data_file):
+                result.extend(parser.parse(data_file_element))
+            return result
         else:
             try:
                 with zipfile.ZipFile(BytesIO(data_file)) as data:
