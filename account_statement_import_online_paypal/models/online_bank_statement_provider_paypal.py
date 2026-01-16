78ec31cd240e9457fea62f3efb87b3cbee2d5355
tendil <mejta.diman@gmail.com>
2026-01-16 11:33:43 +0000

    [FIX] account_statement_import_online_paypal: format PayPal datetimes safely
    This commit fixes PayPal requests failing due to strict datetime schema.
    
    Steps to reproduce:
    - Configure PayPal.com online bank statement provider with valid Client ID/Secret;
    - Open the related "Online Bank Statement Providers -> PayPal" and click *Pull Online Bank Statement*;
    - Select a date range that includes datetimes with microseconds and click *Pull* (or *Show Transaction Data*).
    
    Expected result:
    - PayPal requests are accepted and the statement pull proceeds normally.
    
    Current result:
    - PayPal can reject the request with INVALID_REQUEST because start/end dates include microseconds
      or a schema-incompatible datetime format.
    
    Task: 5208

--- account_statement_import_online_paypal/models/online_bank_statement_provider_paypal.py
+++ account_statement_import_online_paypal/models/online_bank_statement_provider_paypal.py
@@ -183,6 +183,18 @@ class OnlineBankStatementProviderPayPal(models.Model):
             ("paypal", "PayPal.com"),
         ]
 
+    @api.model
+    def _paypal_format_datetime(self, dt_naive_utc):
+        """Format naive UTC datetime for PayPal query params.
+
+        PayPal is picky about schema; microseconds often break requests.
+        Use RFC3339-like format with seconds and Z (UTC).
+        """
+        if not dt_naive_utc:
+            return None
+        dt_naive_utc = dt_naive_utc.replace(microsecond=0)
+        return dt_naive_utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+
     def _obtain_statement_data(self, date_since, date_until):
         self.ensure_one()
         if self.service != "paypal":
@@ -408,14 +420,14 @@ class OnlineBankStatementProviderPayPal(models.Model):
 
     def _paypal_get_transaction(self, token, transaction_id, timestamp):
         self.ensure_one()
-        transaction_date_ini = (timestamp - relativedelta(seconds=1)).isoformat() + "Z"
-        # We want to intentionally force 23:59:59 because in some very specific cases
-        # and without apparent explanation, a transaction that has a specific date and
-        # time defined is not obtained at that time, therefore, we want to ensure that
-        # it will be obtained in this request to avoid false errors.
-        transaction_date_end = (
-            timestamp + relativedelta(hour=23, minute=59, second=59)
-        ).isoformat() + "Z"
+        # Make sure we cover the whole day to avoid PayPal "missing" some transactions
+        ts = timestamp.replace(microsecond=0)
+        transaction_date_ini = self._paypal_format_datetime(
+            ts - relativedelta(seconds=1)
+        )
+        transaction_date_end = self._paypal_format_datetime(
+            ts + relativedelta(hour=23, minute=59, second=59)
+        )
         url = (
             (self.api_base or PAYPAL_API_BASE)
             + "/v1/reporting/transactions"
@@ -458,8 +470,8 @@ class OnlineBankStatementProviderPayPal(models.Model):
                         "&page=%d"
                         % (
                             currency,
-                            interval_start.isoformat() + "Z",
-                            interval_end.isoformat() + "Z",
+                            self._paypal_format_datetime(interval_start),
+                            self._paypal_format_datetime(interval_end),
                             page,
                         )
                     )
