e5011860ccb95c263a251539617f90e77b74a6a1
David Wulliamoz <dwulliamoz@compassion.ch>
2026-03-02 16:59:15 +0100

    fix precommit

--- account_statement_import_camt/models/account_statement_import.py
+++ account_statement_import_camt/models/account_statement_import.py
@@ -91,10 +91,10 @@ class AccountStatementImport(models.TransientModel):
                             )
                         except (ValueError, UserError) as e:
                             _logger.exception(
-                                "Error processing %s in ZIP: %s", member, e
+                                "Error processing file %s in ZIP: %s", member, e
                             )
                             global_result["notifications"].append(
-                                "Error processing file %s in ZIP: %s" % (member, e)
+                                f"Error processing file {member} in ZIP: {e}"
                             )
                 return global_result
         except zipfile.BadZipFile:
