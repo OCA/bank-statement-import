d44d4b780f6ccf865d357b54b756221049a24d65
Bhavesh Heliconia <oca@heliconia.in>
2026-04-06 10:32:11 +0530

    [ADD] account_bank_statement_cascade_delete

---
+++ account_bank_statement_cascade_delete/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Heliconia Solutions Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Bank Statement Cascade Delete",
+    "summary": "Delete bank statement lines along with the bank statement",
+    "version": "18.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/bank-statement-import",
+    "author": "Heliconia Solutions Pvt. Ltd., Odoo Community Association (OCA)",
+    "maintainers": ["Bhavesh Heliconia"],
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "installable": True,
+    "application": False,
+}
---
+++ account_bank_statement_cascade_delete/readme/USAGE.md
@@ -0,0 +1,8 @@
+To use this module, simply:
+
+1. Go to **Accounting > Dashboard**.
+2. Click on the three dots menu of a bank journal.
+3. Go to **Operations > Statements**.
+4. Select a statement and click **Action > Delete**.
+
+The statement and all its lines will be deleted together.
