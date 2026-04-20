9982d6c4e044d15c8f53d5a1282bb6bee1ce6db4
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2026-04-20 09:16:32 -0300

    [MIG] account_statement_import_ofx_by_acctid: Migration to 18.0

--- account_statement_import_ofx_by_acctid/__manifest__.py
+++ account_statement_import_ofx_by_acctid/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Import OFX Bank Statement by ACCTID""",
     "category": "Banking addons",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/bank-statement-import",
